09256d7124b825ca0ebcb6cbd9418f54956d9b7b
ati72 <hegedusattila@protonmail.com>
2022-09-07 12:10:09 +0200

    3rd exercise completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    reversed = "";
+    start = string.length - 1;
 
+    if(string == "") {
+        return reversed;
+    } else {
+        for(i = start; i>=0; i--){
+            reversed += string.charAt(i);
+        }
+        return reversed;
+    }
+
+    
 };
 
 // Do not edit below this line
