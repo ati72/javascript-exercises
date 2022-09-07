5bf5b3478ad6faa203169a4022ea9e9acb269c44
ati72 <hegedusattila@protonmail.com>
2022-09-07 12:30:51 +0200

    4th exercise completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removable) {
+    
+    for(const args of removable )  {
+        let index = array.indexOf(args);
+    if(index > -1) {
+        array.splice(index, 1);
+    }
+    index = "";
+    }
+    return array;
+    
 };
 
 // Do not edit below this line
