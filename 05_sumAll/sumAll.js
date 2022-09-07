a3303f79d443fd021a3562400c57af40a7bbe721
ati72 <hegedusattila@protonmail.com>
2022-09-07 12:45:29 +0200

    5th exercise completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(start, stop) {
+    let sum = 0;
 
+    if(start > stop) {
+        let temp = start;
+        start = stop;
+        stop = temp;
+    } else if(start < 0 || stop < 0) {
+        return "ERROR";
+    } else if(!Number.isInteger(start) || !Number.isInteger(stop)) {
+        return "ERROR";
+    }
+
+    for(i=start; i<=stop; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
