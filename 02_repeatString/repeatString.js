066407f126f7b62ed68ea1c92ddf287471813748
ati72 <hegedusattila@protonmail.com>
2022-09-07 11:56:02 +0200

    2nd exercise completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, numOfTimes) {
+    repeated = "";
+    
+    if(numOfTimes > 0) {
+        for(i=0; i<numOfTimes; i++) {
+            repeated += string;
+        }
+    } else if(numOfTimes < 0) {
+        return "ERROR";
+    } else if(string == ""){
+        return repeated;
+    }
+    
+    return repeated;
 };
 
 // Do not edit below this line
