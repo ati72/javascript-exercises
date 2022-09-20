02d27ca01e40f6eb3768fefe97214be305407286
ati72 <hegedusattila@protonmail.com>
2022-09-20 14:24:55 +0200

    exercise completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    
+        if (n < 0) return "OOPS";
+        if (n === 0) return 0;
+        let a = 0;
+        let b = 1;
+        for (let i = 1; i < n; i++) {
+          const temp = b;
+          b = a + b;
+          a = temp;
+        }
+        return b;
 
 };
 
